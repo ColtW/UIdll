fcde98ce137426f56059c43306a3374bdf2b9b82
Josiah0124864 <josiahemit93@yahoo.com>
2016-12-02 12:33:42 -0600

    Added to the license plate recognition, dont know if it should include anything for an OCR. Added to the vehicle elevator, i added the if statements for when it should activate or not. Dont know if the if statements would work better here or over in the windows form for the elevator console.

--- UIdll/LicensePlateRecognition.cs
+++ UIdll/LicensePlateRecognition.cs
@@ -9,5 +9,71 @@ namespace UIdll
     // Claim for Josiah's Class
     public class LicensePlateRecognition : VehicleElevator
     {
+        // Taking a picture and converting it into a string of letters and numbers
+        // which will then see if it matches with any registered customers or members
+        // if not then they will be asked to input their id, and the license plate will be deemed
+        // a temporary association with the account.
+        // if there is none, they will still be asked for id, but the license plate wont be added
+
+        // Something to represent a Optical camera recognition
+        public object OCR { get; set; }
+
+        public string ElevatorLicenseCam()
+        {
+            // storage for license plate
+            string licensePlate = String.Empty;
+
+            try
+            {
+                // just a placeholder for now
+                OCR = licensePlate;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return licensePlate;
+        }
+
+        public string ExitLicenseCam()
+        {
+            // storage for license plate
+            string licensePlate = String.Empty;
+
+            try
+            {
+                // just a placeholder for now
+                OCR = licensePlate;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return licensePlate;
+        }
+
+        public bool ElevatorOccupancy()
+        {
+            bool occupancy = false;
+
+            try
+            {
+                if(OCR != null)
+                {
+                    occupancy = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            return occupancy;
+        }
     }
 }
--- UIdll/VehicleElevator.cs
+++ UIdll/VehicleElevator.cs
@@ -9,5 +9,142 @@ namespace UIdll
     // Claim For Josiah's Class
     public class VehicleElevator : ParkALot
     {
+        LicensePlateRecognition licensePlate = new LicensePlateRecognition();
+        RegisteredCustomer registeredCustomer = new RegisteredCustomer();
+        CustomerInfo customerInfo = new CustomerInfo();
+
+        public void ElevatorLift()
+        {
+            // determining if they are a registered customer or member
+            try
+            {
+                // checking if they have a license plate
+                if (licensePlate != null)
+                {
+                    // checking if the license plater number found belongs to a registered customer
+                    // or to a member
+                    if (licensePlate = customerInfo.licensePlate)
+                    {
+                        // then checking if they have a reservation at this time
+                        if (customerInfo.id = registeredCustomer.reservation)
+                        {
+                            // succeeded, and they are a customer with a reservation and a license plate
+                            // tied to their account. so the elevator will lift
+                        }
+                        else
+                        {
+                            // if the customer doesnt have a reservation for this time, then it will ask for
+                            // their id and their parking duration to find available spots
+                        }
+                    }
+                    else
+                    {
+                        // if the license plate found does not match with any customers in customerInfo
+                        // then the display will ask for id 
+                        string rawID = String.Empty;
+
+                        Console.ReadLine();
+                        rawID = Console.ReadLine();
+
+                        if (rawID = customerInfo.id)
+                        {
+                            // then it will check for a reservation for this time
+                            if (customerInfo.id = registeredCustomer.reservation)
+                            {
+                                // the customer has a reservation but the car isnt recognized to it will be 
+                                // listed in the past reservations as a temporary license plate
+                                // and then the elevator will lift
+                            }
+                            else
+                            {
+                                // if they didnt have a reservation, then they will be asked for the parking duration 
+                                // and if they have availabilty then they can go up
+                                if()
+                                {
+                                    // then if this is executed it means there is availability for 
+                                    // a parking spot during that duration 
+                                    // so the elevator will lift
+                                }
+                                else
+                                {
+                                    // there is no availability for a parking spot during that duration, so
+                                    // the customer is asked to get off the elevator and search the ground level
+                                    // for a parking spot
+                                    // if they dont get off, then after a set amount of time, the system will alert
+                                    // security
+                                    licensePlate.ElevatorOccupancy();
+
+                                    bool value = licensePlate.ElevatorOccupancy();
+
+                                    if(value == true)
+                                    {
+                                        // then there is somebody there, and security will be called
+                                        // maybe adding a silent timer so they will have time to leave
+                                    }
+                                    else
+                                    {
+                                        // now there is no occupancy, and it will go back to normal
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // this is for when there is no license plate, but it asks for the id anyways
+                    string rawID = String.Empty;
+
+                    Console.ReadLine();
+                    rawID = Console.ReadLine();
+
+                    if (rawID = customerInfo.id)
+                    {
+                        // then it will check for a reservation for this time
+                        if (customerInfo.id = registeredCustomer.reservation)
+                        {
+                            // the customer has a reservation
+                            // and then the elevator will lift
+                        }
+                        else
+                        {
+                            // if they didnt have a reservation, then they will be asked for the parking duration 
+                            // and if they have availabilty then they can go up
+                            if ()
+                            {
+                                // then if this is executed it means there is availability for 
+                                // a parking spot during that duration 
+                                // so the elevator will lift
+                            }
+                            else
+                            {
+                                // there is no availability for a parking spot during that duration, so
+                                // the customer is asked to get off the elevator and search the ground level
+                                // for a parking spot
+                                // if they dont get off, then after a set amount of time, the system will alert
+                                // security
+                                licensePlate.ElevatorOccupancy();
+
+                                bool value = licensePlate.ElevatorOccupancy();
+
+                                if (value == true)
+                                {
+                                    // then there is somebody there, and security will be called
+                                    // maybe adding a silent timer so they will have time to leave
+                                }
+                                else
+                                {
+                                    // now there is no occupancy, and it will go back to normal
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
