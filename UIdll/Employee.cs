dc1d7456494fbf29cf748cba534c8b1968e395bf
The Red Comet <The Red Comet@LONDENION>
2016-12-02 16:05:01 -0600

    Ralph claimed this class

--- UIdll/Employee.cs
+++ UIdll/Employee.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace UIdll
 {
+    //Ralph calls this class
     public class Employee : Person
     {//Colt isn't claiming this class but offering suggestions based on what I did in the Person superclass
      //check out my fields I declared in Person, make an operator object, and set the fields accordingly.
