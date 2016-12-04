9bd459b737489a00399ba5f53b586fb783d777ff
SJReno <spencerreno@gmail.com>
2016-12-04 17:13:45 -0600

    I picked my classes for this project

--- UIdll/RegisteredCustomer.cs
+++ UIdll/RegisteredCustomer.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace UIdll
 {
+    //Spencer claims this class
      public class RegisteredCustomer : Customer
     {//Colt isn't claiming this class but offering suggestions based on what I did in the Person superclass
      //check out my fields I declared in Person, make an operator object, and set the fields accordingly.
+     
     }
 }
