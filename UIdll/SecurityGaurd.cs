1f7ab02373a293e254ac18ca0246e7644870102c
Trevor Bates <Kosoumi@outlook.com>
2016-12-05 14:31:08 -0600

    Trevor B will work on this class.

--- UIdll/SecurityGaurd.cs
+++ UIdll/SecurityGaurd.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace UIdll
 {
+    // Trevor B will take this class on
     public class SecurityGaurd : Employee
     {//Colt isn't claiming this class but offering suggestions based on what I did in the Person superclass
      //check out my fields I declared in Person, make an operator object, and set the fields accordingly.
