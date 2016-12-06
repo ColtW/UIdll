7e79d974cae743b70dc9e0254dc1450877f403b3
monkey <monkey@BB21110>
2016-12-06 12:02:29 -0600

    Built some code for Guest class

--- UIdll/GuestCustomer.cs
+++ UIdll/GuestCustomer.cs
@@ -11,5 +11,32 @@ namespace UIdll
     {//Colt isn't claiming this class but offering suggestions based on what I did in the Person superclass
      //check out my fields I declared in Person, make an operator object, and set the fields accordingly.
 
+        // Feel free to add or make suggestions as needed
+
+
+        private string Name { get; set; }
+        private string LicenseNumber { get; set; }
+        public int securityClearanceLevel = 1;
+
+        public string ParkCar()
+        {
+            return "The guest parks the car in the open space";
+        }
+
+
+
+        public void CheckForOpenSpace()
+        {
+            bool CarSpot = true;
+            if(CarSpot == true)
+            {
+                ParkCar();
+            }
+            else
+            {
+                Console.WriteLine("There were no open spaces");
+            }
+        }
+
     }
 }
