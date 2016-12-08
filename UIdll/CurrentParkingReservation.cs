7a1f93b05bd7b806baa566ed87dd7b245add38a1
Chris <longh@DESKTOP-P6IJB70>
2016-12-07 19:51:41 -0600

    search for reservation

--- UIdll/CurrentParkingReservation.cs
+++ UIdll/CurrentParkingReservation.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data.SqlClient;
+using System.Data.Sql;
+
 namespace UIdll
 {
     public class CurrentParkingReservation : RegisteredCustomer
     {
+        // search database for current reservation with ID from persons class
+
+        SqlConnection connection = new SqlConnection();
+
+        public string reservation()
+        {
+            using (SqlCommand searchPlate = connection.CreateCommand())
+            {
+                // in this method we will finish the code to search and return whether or not the customer has a reservation
+
+                // searchPlate.CommandText = "select * from db_tablename.field where Field = 'exactmatch';";
+            }
+
+            return "";
+        }
+
+        
     }
 }
