cf7eed5b2bf3587f559de377c8adafd27bb79b37
garde <garde@DESKTOP-5G99JIH>
2016-12-08 14:10:22 -0600

    Added Fee method

--- UIdll/Fee.cs
+++ UIdll/Fee.cs
@@ -6,9 +6,38 @@ using System.Threading.Tasks;
 
 namespace UIdll
 {
-    public class Fee : RegisteredCustomer
-    {
-		//Cheryl is claiming this class
-		// I changed something
-    }
+	public class Fee : RegisteredCustomer
+	{
+		//Variables to find time in spot
+		////Cheryl is claiming this class
+		////msdn.microsoft.com
+		private int theTime;
+		private int timeStamp = 0;
+		public string TheTime { get; set; }
+		public int SBill { get; set; }
+		public int LBill { get; set; }
+		private bool standard;
+		private int standardTime;
+		public void GetBill()
+		
+		{
+			if(standard == true)
+				{
+					SBill = 2 * timeStamp;
+				}
+			else if(standard == false)
+				{
+				
+				LBill = 3 * (timeStamp - standardTime);
+				}
+		}	
+		
+	}
+}
+	
+
+
+
+
+    
 }
