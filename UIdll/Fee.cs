c1d781cbc01595a65924db960f8060bc2cd158cb
garde <garde@DESKTOP-5G99JIH>
2016-12-12 13:32:41 -0600

    Retrieved Data Converted stringTime to longTime Calculated fees

--- UIdll/Fee.cs
+++ UIdll/Fee.cs
@@ -5,81 +5,117 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.Sql;
+;
 namespace UIdll
 {
 	public class Fee : RegisteredCustomer
 	{
 
 		////Cheryl is claiming this class
-		// Get the time from the liscense plate reader 
+		// Get the time from the database 
 		// Compare the time to the reservations
 		// Change the times into standard hours and overcharge hours
 		// Set the fees
 		// put into invoice form
 		// email to clients
 
-			// variables
-		private int theTime;
-		private int timeStamp = 0;
+		// variables
+		
+		
 		public string TheTime { get; set; }
-		public int SBill { get; set; }
-		public int LBill { get; set; }
+		public long SBill { get; set; }
+		public long LBill { get; set; }
 		private bool standard;
-		private int standardTime;
-		private int RTimeIn;
-		private int RTimeOut;
-		private int ATimeIn;
-		private int ATimeOut;
-		SqlConnection connection = new SqlConnection();
-		//Server=cis1.actx.edu;Database=project;User Id = db2; Password = db20;
-		//connection.ConnectionString = "Server=cis1.actx.edu;Database=project;User Id=project;Password = project1;";
-		//connection.Open();
-		//using (SqlCommand readAllProfessorRecords = connection.CreateCommand())
-		//          {
-		//              readAllProfessorRecords.CommandText = "select * from db_owner.Professor where FirstName = 'Dewayne';";
+		private string standardTime;
+		private string sRTimeIn;
+		private string sRTimeOut;
+		private string sATimeIn;
+		private string sATimeOut;
+		private string StandardTime;
+		//private long lngRTimeIn;
+		//private long lngRTimeOut;
+		//private long lngATimeIn;
+		//private long lngATimeOut;
+		private long lngRTimeDiff;
+		private long lngATimeDiff;
+		private long detStandardTime;
+		private string Name = "";
+		DateTime lngRTimeIn = DateTime.ParseExact("RTimeIn", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+		DateTime lngRTimeOut = DateTime.ParseExact("RTimeOut", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+		DateTime lngATimeIn = DateTime.ParseExact("ATimeIn", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+		DateTime lngATimeOut = DateTime.ParseExact("ATimeOut", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+		public void ConnectAndCloseDataBase()
+		{
+			SqlConnection connection = new SqlConnection();
+			connection.ConnectionString = "Server=cis1.actx.edu;Database=Project2;User Id=db2;Password = db20;";
+			connection.Open();
+			using (SqlCommand readAllReservationsRecords = connection.CreateCommand())
+			{
+				readAllReservationsRecords.CommandText = "select * from dbo.Reservations where ReservationID = 'Name';";
 
-		//              using (SqlDataReader reader = readAllProfessorRecords.ExecuteReader())
-		//              {
-		//                  string rec = "";
-		//                  while (reader.Read())
-		//                  {
-		//                      RTimeIn = reader.GetString(1);
-		//                      RTimeOut= reader.GetString(2);
-		//                      ATimeIn = reader.GetString(3);
-		//						ATimeOut = reader.GetString(4);
-		//                     
-		//						}
-		//					 }
-		//				}
+				using (SqlDataReader reader = readAllReservationsRecords.ExecuteReader())
+				{
 
-		//public static DateTime FromUnixTime(this long unixTime)
+					while (reader.Read())
+					{
+						sRTimeIn = reader.GetString(3);
+						sRTimeOut = reader.GetString(4);
+						sATimeIn = reader.GetString(7);
+						sRTimeOut = reader.GetString(8);
+
+					}
+				}
+			}
+		}
+		public void ConvertStringtoTime()
+		{
+			//DateTime lngRTimeIn = DateTime.ParseExact("RTimeIn", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+			//DateTime lngRTimeOut = DateTime.ParseExact("RTimeOut", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+			//DateTime lngATimeIn = DateTime.ParseExact("ATimeIn", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+			//DateTime lngATimeOut = DateTime.ParseExact("ATimeOut", "HH:mm tt", System.Globalization.CultureInfo.InvariantCulture);
+			
+		}
+
+		public void DetermineTime()
+		{
+			System.TimeSpan lngRTimeDiff = lngRTimeOut.Subtract(lngRTimeIn);
+			System.TimeSpan lngATimeDiff = lngATimeOut.Subtract(lngATimeIn);
+			System.TimeSpan detStandardTime = lngRTimeDiff.Subtract(lngRTimeDiff);
+		}
+		public void DetermineStandardTime()
+		{
+			if (detStandardTime >= 0)
+			{
+				standard = true;
+			}
+			if (detStandardTime < 0)
+			{
+				standard = false;
+			}
+		}
+		//public bool DetermineStandardTime()
 		//{
-		//	var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-		//	return epoch.AddSeconds(unixTime);
+		//	if (detStandardTime > 0)
+		//	{
+		//		return false;
+		//	}
+		//	else
+		//	{
+		//		return true;
+		//	}
 		//}
-	//public static long ToUnixTime(this DateTime date)
-	//{
-	//	var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-	//	return Convert.ToInt64(date - epoch).TotalSeconds);
-	//}
-	//public bool DetermineStandardTime(standard);
-	//{
-	//	if (ATimeOut >= RTimeOut)
-	//	{
-	//		standard = false;
-	//	}
-	//}
 
 
-	//public void GetBill()
-	//	{
-	//		SBill = 2 * timeStamp;
-	//		if (standard == false)
-	//		{
-	//			LBill = 4 * (timeStamp - standardTime) + SBill;
-	//		}
-	//	}
-}
+		public void GetBill()
+		{
+			SBill = 2 * lngRTimeDiff;
+			if (standard == false)
+			{
+				LBill = 4 * (lngATimeDiff - lngRTimeDiff) + SBill;
+			}
+
+		}
+	}
 }
 	
 
