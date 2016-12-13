6a192ec4c09eed2242778558cd1f68562c410c13
Josiah0124864 <josiahemit93@yahoo.com>
2016-12-13 14:01:53 -0600

    added fee into exitLicenseCam

--- UIdll/LicensePlateRecognition.cs
+++ UIdll/LicensePlateRecognition.cs
@@ -27,7 +27,6 @@ namespace UIdll
             SqlConnection connection = new SqlConnection();
             connection.ConnectionString = "Server=cis1.actx.edu;Database=project2;User Id=db2;Password = db20;";
             connection.Open();
-            Console.WriteLine(connection.ServerVersion);
 
             // storage for license plate
             string licensePlate = String.Empty;
@@ -78,7 +77,8 @@ namespace UIdll
             SqlConnection connection = new SqlConnection();
             connection.ConnectionString = "Server=cis1.actx.edu;Database=project2;User Id=db2;Password = db20;";
             connection.Open();
-            Console.WriteLine(connection.ServerVersion);
+
+            Fee fee = new Fee();
 
             // storage for license plate
             string licensePlate = String.Empty;
@@ -109,6 +109,8 @@ namespace UIdll
                                     updateReservedParking.CommandText = "update dbo.GarageSpaces set Occupied = false;";
                                     updateReservedParking.ExecuteNonQuery();
                                 }
+
+                                fee.GetBill();
 
                                 tryAgain = true;
                             }
